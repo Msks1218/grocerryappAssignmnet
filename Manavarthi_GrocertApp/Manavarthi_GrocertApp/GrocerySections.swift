1121cce91ce3cd037c3f5b930c4ff78fc2e0ee3f
Sanjay krishna Manavarthi <89273866+Msks1218@users.noreply.github.com>
2022-04-05 12:15:18 -0500

    Add files via upload

---
+++ Manavarthi_GrocertApp/Manavarthi_GrocertApp/GrocerySections.swift
@@ -0,0 +1,10 @@
+//
+//  GrocerySections.swift
+//  Manavarthi_GrocertApp
+//
+//  Created by Manavarthi,Sanjay Krishna on 4/5/22.
+//
+
+import Foundation
+
+var section = ""
---
+++ Manavarthi_GrocertApp/Manavarthi_GrocertApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Manavarthi_GrocertApp
+//
+//  Created by Manavarthi,Sanjay Krishna on 4/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var grocerySectionsTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
